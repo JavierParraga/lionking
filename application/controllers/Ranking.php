2ceffacbeb58371a3638be9b0d3631cf672f7b3d
javie <javier.parraga.99@gmail.com>
2021-02-02 02:03:16 +0100

    edfg

--- application/controllers/Ranking.php
+++ application/controllers/Ranking.php
@@ -1,7 +1,7 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-require APPPATH. '/libraries/REST_Controller.PHP';
+<?php
+require APPPATH . '/libraries/REST_Controller.php';
+
 class Ranking extends REST_Controller{
     public function __construct(){
         parent::__construct();
